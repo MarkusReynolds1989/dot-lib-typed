4c2c16af14851496e19bf97158d3233cec749c81
Markus Reynolds <markusreynolds1989@gmail.com>
2022-12-27 19:59:35 -0600

    Added more stdlib

--- array.rkt
+++ array.rkt
@@ -3,7 +3,49 @@
          racket/list
          racket/stream)
 
-(define argument-empty "ArgumentException: Array is empty.")
+; Returns a new array that contains all pairings of elements from the first and second arrays.
+(define (array-all-pairs array-one array-two)
+  (define (loop index)
+    (cond
+      [(= index (array-length array-one)) '()]
+      [(cons (list (array-get index array-one) (array-get index array-two)) (loop (+ index 1)))]))
+  (list->vector (loop 0)))
+
+; Builds a new array that contains elements of the first array followed by the elements of the second array.
+(define (array-append first-array second-array)
+  (vector-append first-array second-array))
+
+; Get the rounded average of the array.
+(define (array-average array)
+  (define len (vector-length array))
+  (define sum (array-sum array))
+  (round (/ sum len)))
+
+;
+(define (array-average-by projection array)
+  (array-average (vector-map projection array)))
+
+; Reads a range of elements from the first array and write them into the second.
+; Mutates the second array with elements from the first.
+; source: 'T[]
+; source-index: int
+; target: 'T[]
+; taget-index: int
+; count: int
+(define (array-blit source source-index target target-index count)
+  0)
+
+; Applies the given fucntion to each element of the array. Returns teh array comprised of the
+; results x for each element where the function returns Some(x).
+; chooser: 'T -> 'U option
+; array: 'T[]
+; Returns 'U[]
+(define (array-choose chooser array)
+  (vector-map chooser array))
+
+; Divides the input array into chunks of size at most chunk-size.
+(define (array-chunk-by-size chunk-size array)
+  (define (loop )))
 
 ; Gets the first element of the array.
 (define (array-head array)
@@ -20,16 +62,11 @@
 (define (array-length array)
   (vector-length array))
 
-(define (array-length-check array-one array-two)
-  (when (not (= (vector-length array-one) (vector-length array-two)))
-    (raise "Argument Exception")))
-
 (define (array-iter action array)
   (for ([i (range 0 (vector-length array))])
     (action (array-get i array))))
 
 (define (array-iter-two action array-one array-two)
-  (array-length-check array-one array-two)
   (for ([i (range 0 (vector-length array-one))])
     (action (array-get i array-one) (array-get i array-two))))
 
@@ -43,8 +80,6 @@
 ; Returns the greatest of all elements of the array.
 ; TODO: Some sort of comparator for different types, this will only work for numerics.
 (define (array-max array)
-  (when (vector-empty? array)
-    (raise argument-empty))
   (define (loop array index max)
     (cond
       [(= index (array-length array)) max]
@@ -55,18 +90,12 @@
 ; Returns the greatest of all elements of the array.
 ; TODO: Contracts on types and comparator.
 (define (array-max-by projection array)
-  (when (vector-empty? array)
-    (raise argument-empty))
-  (define (loop projection array index max)
+  (define (loop index max)
     (cond
       [(= index (array-length array)) max]
-      [(projection array) (loop projection array (+ 1 index) (array-get index array))]
-      [else (loop projection array (+ 1 index) max)]))
-  (loop projection array 0 (array-head array)))
-
-; Builds a new array that contains elements of the first array followed by the elements of the second array.
-(define (array-append first-array second-array)
-  (vector-append first-array second-array))
+      [(projection array) (loop (+ 1 index) (array-get index array))]
+      [else (loop (+ 1 index) max)]))
+  (loop 0 (array-head array)))
 
 ; Applies a function to each element of the collection, threading an accumulator argument through
 ; the computation. If the input function is f and the elements are i0...iN then computers
@@ -83,16 +112,6 @@
 ; Gets an element from an array.
 (define (array-get index array)
   (vector-ref array index))
-
-; Sum all the elements of the array together.
-(define (array-sum array)
-  (array-fold (lambda (x y) (+ x y)) 0 array))
-
-; Get the rounded average of the array.
-(define (array-average array)
-  (define len (vector-length array))
-  (define sum (array-sum array))
-  (round (/ sum len)))
 
 (define (array-of-list list)
   (list->vector list))
@@ -118,12 +137,33 @@
 (define (array-sort array)
   (vector-sort array <))
 
+(define (array-sub array start-index count)
+  (vector-take (vector-drop array start-index) count))
+
+; Sum all the elements of the array together.
+(define (array-sum array)
+  (array-fold (lambda (x y) (+ x y)) 0 array))
+
+(define (array-tail array)
+  (vector-drop array 1))
+
+(define (array-take count array)
+  (vector-take array count))
+
+(define (array->list array)
+  (vector->list array))
+
+(define (array->seq array)
+  (stream-first (stream (array->list array))))
+
 (module+ test
   (require rackunit)
   (define first-array #(1 2 3 4))
   (define second-array #(5 6 7 8))
   (define combined-arrays #(1 2 3 4 5 6 7 8))
 
+  ; All-pairs test
+  (check-equal? (array-all-pairs first-array second-array) (vector '(1 5) '(2 6) '(3 7) '(4 8)))
   ; Append test
   (check-equal? combined-arrays (array-append first-array second-array))
   ; Sum test
@@ -145,4 +185,14 @@
   ; Skip test
   (check-equal? #(3 4) (array-skip 2 #(1 2 3 4)))
   ; Sort test
-  (check-equal? #(1 2 3 4) (array-sort #(4 2 3 1))))
+  (check-equal? (array-sort #(4 2 3 1)) #(1 2 3 4))
+  ; Sub test
+  (check-equal? (array-sub #(1 2 3 4) 1 2) #(2 3))
+  ; Tail test
+  (check-equal? (array-tail combined-arrays) #(2 3 4 5 6 7 8))
+  ; Take test
+  (check-equal? (array-take 2 combined-arrays) #(1 2))
+  ; ->list test
+  (check-equal? (array->list first-array) (list 1 2 3 4))
+  ; ->seq test
+  (check-equal? (stream->list (array->seq first-array)) (stream->list (stream 1 2 3 4))))
