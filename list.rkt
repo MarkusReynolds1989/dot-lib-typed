2e9404093cf48a62b30e8902edf71bf44139ee46
Markus Reynolds <markusreynolds1989@gmail.com>
2022-12-27 21:38:34 -0600

    Started work on typed list library to match F#

--- list.rkt
+++ list.rkt
@@ -1,5 +1,12 @@
 #lang typed/racket/base
 
+(: list-all-pairs (All (T) (-> (Listof T) (Listof T) (Listof (Listof T)))))
+(define (list-all-pairs list-one list-two)
+  (cond
+    [(null? (cdr list-one)) (cons (list (car list-one) (car list-two)) '())]
+    [else
+     (cons (list (car list-one) (car list-two)) (list-all-pairs (cdr list-one) (cdr list-two)))]))
+
 (: list-append (All (T) (-> (Listof T) (Listof T) (Listof T))))
 (define (list-append first-list second-list)
   (append first-list second-list))
@@ -10,7 +17,7 @@
 
 (: list-choose (All (T U) (-> (-> T (Option U)) (Listof T) (Listof U))))
 (define (list-choose chooser input)
-  ())
+  '())
 
 (module+ test
   (require typed/rackunit)
