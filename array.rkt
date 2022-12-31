bb3dcc570382a7f56b355cedbef2ffd47b2c9b94
Markus Reynolds <markusreynolds1989@gmail.com>
2022-12-31 12:20:52 -0600

    Type checking is weird

--- array.rkt
+++ array.rkt
@@ -1,5 +1,16 @@
-#lang typed/racket/base
+#lang typed/racket
 
+; TODO: array-all-pairs
+
+(: array-append (All (T) (-> (Vectorof T) (Vectorof T) (Vectorof T))))
+(define (array-append array-one array-two)
+  (let ([new-array (make-vector (+ (vector-length array-one) (vector-length array-two))
+                                (vector-ref array-one 1))])
+    (vector-copy! new-array 0 array-one 0 (vector-length array-one))
+    (vector-copy! new-array (vector-length array-one) array-two 0 (vector-length array-two))
+    new-array))
 
 (module+ test
-  (require rackunit))
+  (require typed/rackunit)
+
+  (check-equal? (array-append #(1 2 3) #(4 5 6)) #(1 2 3 4 5 6)))
--- test.ps1
+++ test.ps1
@@ -1,2 +1,5 @@
-raco make list.rkt
-raco test list.rkt
+#raco make list.rkt
+#raco test list.rkt
+
+raco make array.rkt
+raco test array.rkt
