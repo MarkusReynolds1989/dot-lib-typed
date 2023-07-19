2e125ff95dbfcda8a09e10bc20916e6f648791c4
Markus <markusreynolds1989@gmail.com>
2023-07-19 06:32:46 -0500

    Added notes.

--- src/io/file.rkt
+++ src/io/file.rkt
@@ -16,5 +16,9 @@
   (define simple_multi_line "simple_multi_line.txt")
 
   (test-equal? "read-all-lines test" (list "1" "2" "3" "4") (read-all-lines simple_multi_line))
+
+  ; Git changes the line endings on upload so this will fail.
   (test-equal? "read-all-bytes test" #"1\r\n2\r\n3\r\n4\r\n" (read-all-bytes simple_multi_line))
+
+  ; Git changes the line endings on upload so this will fail.
   (test-equal? "read-all-text test" "1\r\n2\r\n3\r\n4\r\n" (read-all-text simple_multi_line)))
