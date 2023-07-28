be6e5f487413851b2a75344eba7bb3b2111def6f
Markus <markusreynolds1989@gmail.com>
2023-07-27 21:31:44 -0500

    Updated build and added.

---
+++ build.rkt
@@ -0,0 +1,12 @@
+#lang racket
+
+(define packages
+  (list "src/globals.rkt"
+        "src/collections/map.rkt"
+        "src/collections/list.rkt"
+        "src/collections/array.rkt"
+        "src/collections/seq.rkt"
+        "src/io/file.rkt"))
+
+(for-each (lambda (x) (system (~a "raco make " x))) packages)
+(for-each (lambda (x) (system (~a "raco test " x))) packages)
