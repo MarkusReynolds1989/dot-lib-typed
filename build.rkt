477e7b23540b2417a56c52d26b6cb383f1bac874
Markus <markusreynolds1989@gmail.com>
2023-07-27 21:35:53 -0500

    Try to hit version.

--- build.rkt
+++ build.rkt
@@ -8,5 +8,5 @@
         "src/collections/seq.rkt"
         "src/io/file.rkt"))
 
-(for-each (lambda (x) (system (~a "raco make " x))) packages)
+(for-each (lambda (x) (system (~a "raco make " x)) (displayln (~a "Building: " x)) packages)
 (for-each (lambda (x) (system (~a "raco test " x))) packages)
